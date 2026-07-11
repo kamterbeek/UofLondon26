369254194d4a90bfb0608f4999e5be7c35409eba
Kassandra <kam.terbeek@gmail.com>
2026-07-10 22:21:21 -0600

    Add tests for Task API to verify task retrieval

--- test.js
+++ test.js
@@ -1,1 +1,27 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 
+const app = require("../server");
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe("Task API", () => {
+
+    it("should return all tasks", (done) => {
+
+        chai.request(app)
+            .get("/tasks")
+            .end((err, res) => {
+
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an("array");
+
+                done();
+
+            });
+
+    });
+
+});
