98aa745a78fb776e87bca9d504521c2e97a422f6
Kassandra <kam.terbeek@gmail.com>
2026-07-10 22:19:45 -0600

    Start server only if module is main

--- server.js
+++ server.js
@@ -112,6 +112,10 @@ app.delete("/tasks/:id", (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
